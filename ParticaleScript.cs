973e2b52513f13aa56298ba5ac9cbda8461f3c8b
GalaxyBlitz <akbaksh02@hotmail.com>
2017-09-04 18:34:34 +0100

    Create ParticaleScript.cs

---
+++ ParticaleScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParticaleScript : MonoBehaviour {
+
+    private ParticleSystem ps; 
+
+	// Use this for initialization
+	void Start () {
+
+        ps = GetComponent<ParticleSystem>(); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!ps.isPlaying)
+        {
+            Destroy(gameObject); 
+        }
+	
+	}
+}
